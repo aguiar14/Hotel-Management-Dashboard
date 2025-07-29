6470ebddcad7799b3e8e54926896b88cde6de2de
Daniel Aguiar <daniel.aguiar@isidors.com>
2025-07-29 07:52:10 +0300

    [INIT] Setup Angular project -- done

---
+++ Frontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected readonly title = signal('Frontend');
+}
